5e2b48d4a71e64c495c65dd8fcd8279fe91e27f8
Viktoria <v.p.persuk@gmail.com>
2023-10-27 21:18:18 +0300

    Реализованы функции decrypt и encrypt

--- src/lab2/caesar.py
+++ src/lab2/caesar.py
@@ -1,3 +1,7 @@
+# coding= utf-8
+import unittest
+
+# шифрование = смещение на shift символов вперед --> + offset
 def encrypt_caesar(plaintext: str, shift: int = 3) -> str:
     """
     Encrypts plaintext using a Caesar cipher.
@@ -11,22 +15,48 @@ def encrypt_caesar(plaintext: str, shift: int = 3) -> str:
     ''
     """
     ciphertext = ""
-    # PUT YOUR CODE HERE
+    for char in plaintext:
+        # маленькие латинские буквы
+        if 'a' <= char <= 'z':
+            # определение базовой точки
+            offset = ord('a')
+            ciphertext += chr(((ord(char) - offset + shift) % 26) + offset)
+        # большие латинские буквы
+        elif 'A' <= char <= 'Z':
+            # определение базовой точки
+            offset = ord('A')
+            ciphertext += chr(((ord(char) - offset + shift) % 26) + offset)
+        # все остальные символы
+        else:
+            ciphertext += char
     return ciphertext
 
-
+# дешифрование = смещение на shift символов назад --> - offset
 def decrypt_caesar(ciphertext: str, shift: int = 3) -> str:
     """
     Decrypts a ciphertext using a Caesar cipher.
-    >>> decrypt_caesar("SBWKRQ")
-    'PYTHON'
-    >>> decrypt_caesar("sbwkrq")
-    'python'
-    >>> decrypt_caesar("Sbwkrq3.6")
-    'Python3.6'
+    >>> decrypt_caesar("ABC")
+    'XYZ'
+    >>> decrypt_caesar("abc")
+    'xyz'
+    >>> decrypt_caesar("Khoor, Zruog!")
+    'Hello, World!'
     >>> decrypt_caesar("")
     ''
     """
     plaintext = ""
-    # PUT YOUR CODE HERE
+    for char in ciphertext:
+        # маленькие латинские буквы
+        if 'a' <= char <= 'z':
+            # определение базовой точки
+            offset = ord('a')
+            plaintext += chr(((ord(char) - offset - shift) % 26) + offset)
+        # большие латинские буквы
+        elif 'A' <= char <= 'Z':
+            # определение базовой точки
+            offset = ord('A')
+            plaintext += chr(((ord(char) - offset - shift) % 26) + offset)
+        # все остальные символы
+        else:
+            plaintext += char
     return plaintext
